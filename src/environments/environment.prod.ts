7d32a5f8e8f3ffe5afa13135da8530fec5fa5e00
juancte1986 <juan.86.12@hotmail.com>
2020-04-29 18:14:10 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   title: 'GestionaTusReclamos',
-  backendUrl: 'https://gtr-frontend.herokuapp.com/send',
+  backendUrl: 'https://gtr-frontend.herokuapp.com',
   authUrl: '',
 };
