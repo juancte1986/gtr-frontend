5eb5cb25cd45680f896fd2174a0828407518a144
jpveron <jpveron@fleni.org.ar>
2020-05-06 13:10:45 -0300

    Update home.component.html

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -47,7 +47,10 @@ export class HomeComponent implements OnInit, AfterViewInit {
     this.emailService.sendMail({email: this.email, name: this.name, message: this.message}).subscribe(() => {
       this.messageService.add({severity: 'success', summary: 'Servicio de Email', detail: 'El correo se envio correctamente.'});
       this.sending = false;
-    }, (err) => {
+      this.email = '';
+      this.name = '';
+      this.message = '';
+     }, (err) => {
       console.log(err);
       this.sending = false;
       this.messageService.add({severity: 'error', summary: 'Servicio de Email', detail: 'El correo no se envio correctamente.'});
